3c27640fbdfd063cc6958bbfc3660849278a93b4
Munshi Md Arif <63009387+munshi-md-arif@users.noreply.github.com>
2025-08-29 23:24:50 +0530

    Handle single number input

--- test/string_calculator_tdd_kata_test.dart
+++ test/string_calculator_tdd_kata_test.dart
@@ -7,5 +7,9 @@ void main() {
       final calculator = StringCalculator();
       expect(calculator.add(''), equals(0));
     });
+    test('returns the number when a single number is provided', () {
+      final calculator = StringCalculator();
+      expect(calculator.add('1'), equals(1));
+    });
   });
 }
