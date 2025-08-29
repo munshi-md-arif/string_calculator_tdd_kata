ac54e58d4335c7a2cd55a15e509e7a490c0f7aff
Munshi Md Arif <63009387+munshi-md-arif@users.noreply.github.com>
2025-08-29 22:36:22 +0530

    first commit

---
+++ bin/string_calculator_tdd_kata.dart
@@ -0,0 +1,5 @@
+import 'package:string_calculator_tdd_kata/string_calculator_tdd_kata.dart' as string_calculator_tdd_kata;
+
+void main(List<String> arguments) {
+  print('Hello world: ${string_calculator_tdd_kata.calculate()}!');
+}
---
+++ test/string_calculator_tdd_kata_test.dart
@@ -0,0 +1,8 @@
+import 'package:string_calculator_tdd_kata/string_calculator_tdd_kata.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
