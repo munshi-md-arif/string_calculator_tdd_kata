ae90a6dee03e263dddde3c3fd37b64d30259736b
Munshi Md Arif <63009387+munshi-md-arif@users.noreply.github.com>
2025-08-29 23:28:53 +0530

    Handles any number of inputs

--- test/string_calculator_tdd_kata_test.dart
+++ test/string_calculator_tdd_kata_test.dart
@@ -15,5 +15,9 @@ void main() {
       final calculator = StringCalculator();
       expect(calculator.add('1,5'), equals(6));
     });
+    test('handles any amount of numbers', () {
+      final calculator = StringCalculator();
+      expect(calculator.add('1,2,3,4'), equals(10));
+    });
   });
 }
